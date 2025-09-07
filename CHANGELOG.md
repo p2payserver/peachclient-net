67e1bde69a759b058fa5c8ca3ac1c1552357a8fe
0x4C6F6B69 <0x4C6F6B69@proton.me>
2025-09-07 18:20:35 +0200

    Implemented Get_BTC_unit_price_in_EUR test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+- Implemented `Get_BTC_unit_price_in_EUR` test.
+
 ## [0.1.4-preview] - 2025-09-06
 
 - Implemented `GetOfferAsync` method. 
---
+++ tests/Tests/PeachApiClient/_MoreTests.cs
@@ -0,0 +1,21 @@
+﻿using PeachClient;
+using PeachClient.Models;
+using SharpX;
+using SharpX.Extensions;
+using Xunit.Abstractions;
+
+namespace PeachApiClientTests;
+
+public class _MoreTests(ITestOutputHelper output)
+{
+    [Fact]
+    public async Task Get_BTC_unit_price_in_EUR()
+    {
+        PeachApiClient client = Factory.CreatePeachClient();
+
+        var result = await client.GetBtcMarketPriceAsync("EUR");
+
+        Assert.Equal(MaybeType.Just, result.Tag);
+        Assert.True(result.FromJust()!.Price > 0);
+    }
+}
