7555d847ba9abe4a580159b21392e246d18a1f8c
ZeroEx <0x4C6F6B69@proton.me>
2025-10-04 17:18:37 +0000

    Implemented authenticated REST call to create a new offer (#2)
    * Refactored registration and authentication methods
    
    * Implemented GetIdentity method
    
    * Implemented UpdateUserAccount method
    
    * Implemented CreateOffer method
    
    * Enabled RegTest endpoint in tests project
    
    * Fix: added missing suffix to some PeachApiClient methods
    
    * Implemented check against an empty access token
    
    * Implemented Register_account test method

--- src/Core/Models/User.cs
+++ src/Core/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace PeachClient.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeachClient.Models;
 
 public record Disputes(
     int Opened,
@@ -9,7 +11,7 @@ public record Disputes(
 public sealed class User
 {
     public string Id { get; set; }
-    public DateTime CreationDate { get; set; }  
+    public DateTime CreationDate { get; set; }
     public int Trades { get; set; }
     public double Rating { get; set; }
     public double HistoryRating { get; set; }
@@ -21,4 +23,55 @@ public sealed class User
     public Disputes Disputes { get; set; }
     public string? PgpPublicKey { get; set; }
     public string? PgpPublicKeyProof { get; set; }
+}
+
+public sealed class UpdateUserRequest : IValidatableObject
+{
+    public string? PgpPublicKey { get; set; }
+
+    /// <summary>
+    /// Required if <see cref="PgpPublicKey"/> is provided.
+    /// </summary>
+    public string? Message { get; set; }
+
+    /// <summary>
+    /// Required if <see cref="PgpPublicKey"/> is provided.
+    /// </summary>
+    public string? PgpSignature { get; set; }
+
+    /// <summary>
+    /// Required if <see cref="PgpPublicKey"/> is provided.
+    /// </summary>
+    public string? Signature { get; set; }
+
+    public string? FcmToken { get; set; }
+
+    public string? ReferralCode { get; set; }
+
+    /// <summary>
+    /// Can be a fixed numeric fee rate or one of the string values like 'fastestFee'.
+    /// </summary>
+    public string? FeeRate { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        List<ValidationResult> validations = [];
+
+        if (PgpPublicKey != null)
+        {
+            if (Message == null)
+                validations.Add(new ValidationResult($"{nameof(Message)} is required when {nameof(PgpPublicKey)} is provided",
+                    [nameof(Message)]));
+
+            if (PgpSignature == null)
+                validations.Add(new ValidationResult($"{nameof(PgpSignature)} is required when {nameof(PgpPublicKey)} is provided",
+                    [nameof(PgpSignature)]));
+
+            if (Signature == null)
+                validations.Add(new ValidationResult($"{nameof(Signature)} is required when {nameof(PgpPublicKey)} is provided",
+                    [nameof(Signature)]));
+        }
+
+        return validations;
+    }
 }
--- tests/Tests/DelayAttribute.cs
+++ tests/Tests/DelayAttribute.cs
@@ -3,15 +3,10 @@ using Xunit.Sdk;
 
 public class DelayAttribute : BeforeAfterTestAttribute
 {
-    private readonly int _milliseconds;
-
-    public DelayAttribute(int milliseconds)
-    {
-        _milliseconds = milliseconds;
-    }
-
     public override void After(MethodInfo methodUnderTest)
     {
-        Thread.Sleep(_milliseconds);
+        if (TestsConfig.UseRegTestUri) return;
+
+        Thread.Sleep(TestsConfig.DelayForProdApiMs);
     }
 }
