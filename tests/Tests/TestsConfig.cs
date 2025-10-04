c2ca320bd636e9a11c831e65d411f71823917f1d
ZeroEx <0x4C6F6B69@proton.me>
2025-10-04 19:08:25 +0200

    Implemented Register_account test method

--- tests/Tests/TestsConfig.cs
+++ tests/Tests/TestsConfig.cs
@@ -5,4 +5,8 @@ public static class TestsConfig
 
     public static TimeSpan DelayForProdApiMs =>
         TimeSpan.FromDays(Convert.ToDouble(Environment.GetEnvironmentVariable("DelayForProdApiMs")));
+
+    public static string PublicKey => Environment.GetEnvironmentVariable("PublicKey")!;
+    public static string Message => Environment.GetEnvironmentVariable("Message")!;
+    public static string Signature => Environment.GetEnvironmentVariable("Signature")!;
 }
